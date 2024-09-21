da51d562d8a5c5c8968589031dd268625351fb24
samerxxxalsudani <samerxxxalsudani@gmail.com>
2024-09-21 18:49:30 -0500

    Updates

--- client/android/app/src/main/java/com/betafierfast/app/MainActivity.kt
+++ client/android/app/src/main/java/com/betafierfast/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.betafier.app
+package com.BetafierFast.app
 
 import android.os.Build
 import android.os.Bundle
--- client/android/app/src/main/java/com/betafierfast/app/MainApplication.kt
+++ client/android/app/src/main/java/com/betafierfast/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.betafier.app
+package com.BetafierFast.app
 
 import android.app.Application
 import android.content.res.Configuration
